c67568c34de3be16e921f3345cc3b17ef5a3ee69
cdsoftw <cdsoftworks@gmail.com>
2026-02-06 00:23:53 -0600

    chore: configure base URL + timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,11 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 10_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://www.gamesforthebrain.com/game/checkers',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
