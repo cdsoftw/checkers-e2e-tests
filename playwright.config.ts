8f624432b3cfd94b21a454233f498835d72a69c3
cdsoftw <cdsoftworks@gmail.com>
2026-02-06 00:55:49 -0600

    fix: incorrect base URL
    also bump expect timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,10 +24,13 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   timeout: 10_000,
+  expect: {
+    timeout: 10_000,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://www.gamesforthebrain.com/game/checkers',
+    baseURL: 'https://www.gamesforthebrain.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
